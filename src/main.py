a7947a6a9b9d2e8d21febcdbab7fd81ad6e9cd83
Mifa43 <mifa43kotez@gmail.com>
2022-03-01 11:21:00 +0100

    novi endpoint koji ce upisivati u bazu

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, HTTPException
 import logging, uvicorn
 import requests
-
+from models import *
+from crud import *
 # kreiranje logera https://docs.python.org/3/library/logging.html
 logger = logging.getLogger(__name__) 
 logger.setLevel("DEBUG")
@@ -21,6 +22,8 @@ logger.addHandler(ch)
 
 app = FastAPI()
 
+#https://api.openweathermap.org/data/2.5/weather?lat=44.0165&lon=21.0059&appid=cc48c29524a69fb60ffad1e23ec4184d
+
 @app.get("/")
 async def health_check():
     logger.info("{Health : OK}, 200")
@@ -28,10 +31,14 @@ async def health_check():
 
 @app.get("/get-data")
 async def get_data():
-    r = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=44.0165&lon=21.0059&appid=cc48c29524a69fb60ffad1e23ec4184d')
+    r = requests.get('https://api.openweathermap.org/data/2.5/forecast?q=belgrade&appid=cc48c29524a69fb60ffad1e23ec4184d&cnt=30')
     
     logger.info("{0}".format(r.text))
     return {"data": "ok"}
+
+@app.post("/bulk-data")
+async def get_data():
+    pass
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, loop="asyncio")
